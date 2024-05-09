e0d6c56eb0fe3df9a8873fd7e81b77189e1cbd08
MikeLippincott <1michaell2017@gmail.com>
2024-05-08 18:56:57 -0600

    add null distribution calculations

--- 6.analyze_features/scripts/8a.mean_aggregated_PCA_calculate_mahalanobis_distance.py
+++ 6.analyze_features/scripts/8a.mean_aggregated_PCA_calculate_mahalanobis_distance.py
@@ -10,9 +10,13 @@
 import itertools
 import pathlib
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
+import tqdm
 from scipy.spatial.distance import mahalanobis
+from scipy.stats import ttest_ind
 
 # In[2]:
 
@@ -137,9 +141,133 @@ print(
 )
 
 
-# ### Show the mahalanobis distance for each genotype
+# ## Calculate the Mahalanobis distance for random shuffled data points
 
 # In[8]:
+
+
+# from the distribution of each PCA component, randomly sample 1000 points and calculate the mahalanobis distance
+# get the distribution of each PCA component
+high_severity_pca_component_distribution_std = high_severity.describe().loc["std"]
+low_severity_pca_component_distribution_std = low_severity.describe().loc["std"]
+wt_pca_component_distribution_std = wt.describe().loc["std"]
+
+high_severity_pca_component_distribution_mean = high_severity.describe().loc["mean"]
+low_severity_pca_component_distribution_mean = low_severity.describe().loc["mean"]
+wt_pca_component_distribution_mean = wt.describe().loc["mean"]
+
+
+# In[9]:
+
+
+# do 1000 trials of sampling from each PCA component distribution
+# from the distribution of each PCA component, randomly sample 1000 points and calculate the mahalanobis distance
+
+# set the output mean Mahalanobis distance lists per genotype
+mean_high_severity_sampled_mahalanobis_distances_from_trials = []
+mean_low_severity_sampled_mahalanobis_distances_from_trials = []
+mean_wt_sampled_mahalanobis_distances_from_trials = []
+
+# sample 1000 points from the distribution of each PCA component
+num_pca_components = high_severity_pca_component_distribution_std.shape[0]
+num_samples = 100
+num_trials = 1000
+
+for trial in tqdm.tqdm(range(num_trials)):
+    high_severity_sampled_points = pd.DataFrame()
+    low_severity_sampled_points = pd.DataFrame()
+    wt_sampled_points = pd.DataFrame()
+
+    for i in range(num_pca_components):
+        high_severity_sampled_points[f"PC{i+1}"] = np.random.normal(
+            loc=high_severity_pca_component_distribution_mean[i],
+            scale=high_severity_pca_component_distribution_std[i],
+            size=num_samples,
+        )
+        low_severity_sampled_points[f"PC{i+1}"] = np.random.normal(
+            loc=low_severity_pca_component_distribution_mean[i],
+            scale=low_severity_pca_component_distribution_std[i],
+            size=num_samples,
+        )
+        wt_sampled_points[f"PC{i+1}"] = np.random.normal(
+            loc=wt_pca_component_distribution_mean[i],
+            scale=wt_pca_component_distribution_std[i],
+            size=num_samples,
+        )
+
+    inv_cov_matrix = np.linalg.inv(
+        np.cov(high_severity_sampled_points.to_numpy(), rowvar=False)
+    )
+    # calculate the mahalanobis distance for each point within each genotype
+    high_severity_sampled_mahalanobis_distances = []
+    # calculate the mahalanobis distance for each point combination
+    for point in itertools.combinations(high_severity_sampled_points.to_numpy(), 2):
+        distance = mahalanobis(point[0], point[1], inv_cov_matrix)
+        if np.isnan(distance):  # Check for NaN values
+            continue
+        high_severity_sampled_mahalanobis_distances.append(distance)
+
+    mean_high_severity_sampled_mahalanobis_distance = np.mean(
+        high_severity_sampled_mahalanobis_distances
+    )
+
+    inv_cov_matrix = np.linalg.inv(
+        np.cov(low_severity_sampled_points.to_numpy(), rowvar=False)
+    )
+    # calculate the mahalanobis distance for each point within each genotype
+    low_severity_sampled_mahalanobis_distances = []
+    # calculate the mahalanobis distance for each point combination
+    for point in itertools.combinations(low_severity_sampled_points.to_numpy(), 2):
+        distance = mahalanobis(point[0], point[1], inv_cov_matrix)
+        if np.isnan(distance):  # Check for NaN values
+            continue
+        low_severity_sampled_mahalanobis_distances.append(distance)
+
+    mean_low_severity_sampled_mahalanobis_distance = np.mean(
+        low_severity_sampled_mahalanobis_distances
+    )
+
+    inv_cov_matrix = np.linalg.inv(np.cov(wt_sampled_points.to_numpy(), rowvar=False))
+    # calculate the mahalanobis distance for each point within each genotype
+    wt_sampled_mahalanobis_distances = []
+    # calculate the mahalanobis distance for each point combination
+    for point in itertools.combinations(wt_sampled_points.to_numpy(), 2):
+        distance = mahalanobis(point[0], point[1], inv_cov_matrix)
+        if np.isnan(distance):  # Check for NaN values
+            continue
+        wt_sampled_mahalanobis_distances.append(distance)
+
+    mean_wt_sampled_mahalanobis_distance = np.mean(wt_sampled_mahalanobis_distances)
+
+    # append the average mahalanobis distance from the trial to the list for the trial per genotype
+    mean_high_severity_sampled_mahalanobis_distances_from_trials.append(
+        mean_high_severity_sampled_mahalanobis_distance
+    )
+    mean_low_severity_sampled_mahalanobis_distances_from_trials.append(
+        mean_low_severity_sampled_mahalanobis_distance
+    )
+    mean_wt_sampled_mahalanobis_distances_from_trials.append(
+        mean_wt_sampled_mahalanobis_distance
+    )
+
+
+# In[10]:
+
+
+print(
+    f"The high Mahalonobis distance for {num_trials} trials is: {round(np.mean(mean_high_severity_sampled_mahalanobis_distances_from_trials),0)}"
+)
+print(
+    f"The low Mahalonobis distance for {num_trials} trials is: {round(np.mean(mean_low_severity_sampled_mahalanobis_distances_from_trials),0)}"
+)
+print(
+    f"The wt Mahalonobis distance for {num_trials} trials is: {round(np.mean(mean_wt_sampled_mahalanobis_distances_from_trials),0)}"
+)
+
+
+# ### Show the mahalanobis distance for each genotype
+
+# In[11]:
 
 
 print(
@@ -147,3 +275,74 @@ print(
 )
 print("Mahalanobis distance for Low-Severity: ", mean_low_severity_mahalanobis_distance)
 print("Mahalanobis distance for Wild Type: ", mean_wt_mahalanobis_distance)
+
+
+# In[12]:
+
+
+# hypothesis testing the difference between the Mahalanobis distance of the sampled points and the actual points
+# calculate the p-value for the difference between the Mahalanobis distance of the sampled points and the actual points
+
+high_severity_p_value = ttest_ind(
+    high_severity_mahalanobis_distances,
+    mean_high_severity_sampled_mahalanobis_distances_from_trials,
+).pvalue
+low_severity_p_value = ttest_ind(
+    low_severity_mahalanobis_distances,
+    mean_low_severity_sampled_mahalanobis_distances_from_trials,
+).pvalue
+wt_p_value = ttest_ind(
+    wt_mahalanobis_distances, mean_wt_sampled_mahalanobis_distances_from_trials
+).pvalue
+
+print(
+    f"The p-value for the difference between the Mahalanobis distance of the sampled points and the actual points for the High-Severity genotype is {high_severity_p_value}"
+)
+print(
+    f"The p-value for the difference between the Mahalanobis distance of the sampled points and the actual points for the Low-Severity genotype is {low_severity_p_value}"
+)
+print(
+    f"The p-value for the difference between the Mahalanobis distance of the sampled points and the actual points for the Wild Type genotype is {wt_p_value}"
+)
+
+
+# In[13]:
+
+
+# Visualization of the last trial's sampled points
+
+# annotate the genotypes of the sampled points
+high_severity_sampled_points["Metadata_genotype"] = "High-Severity"
+low_severity_sampled_points["Metadata_genotype"] = "Mid-Severity"
+wt_sampled_points["Metadata_genotype"] = "Wild Type"
+# concat the genotype sampled points
+sampled_points = pd.concat(
+    [wt_sampled_points, low_severity_sampled_points, high_severity_sampled_points]
+)
+
+# plot the first 2 PCA components from the actual data
+# set up subplots
+plt.figure(figsize=(10, 5))
+# subplot 1
+plt.subplot(1, 2, 1)
+sns.scatterplot(
+    x="PC1",
+    y="PC2",
+    data=mean_aggregated_data_pca,
+    hue="Metadata_genotype",
+    alpha=0.5,
+)
+plt.title("Real PCA")
+
+# plot the first 2 PCA components from the sampled data
+# subplot 2
+plt.subplot(1, 2, 2)
+sns.scatterplot(
+    x="PC1",
+    y="PC2",
+    data=sampled_points,
+    hue="Metadata_genotype",
+    alpha=0.5,
+)
+plt.title("Sampled PCA")
+plt.show()
